fd0f4d2015c358f996e8cb68efdfdace4cd8f1bc
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2020-02-07 22:04:31 +0530

    Completed the Clock Project

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-var port = 8080;
-// app.use(''express.static(__dirname + '/clock.html'));
-app.use(express.static(__dirname + '/'));
-app.listen(port);
 
-console.log('Starting server at' + port);
+app.use(express.static(__dirname + '/'));
+app.listen(8080);
+
+console.log('Starting server at: ' + 8080);
